be1bce537144e36dc8ce7f193f4dc14fc4f12ed7
vietanhlee <levietanhtrump@gmail.com>
2025-09-13 14:25:43 +0700

    xoá một số các file lỡ commit

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-ames']) > 0 and len(res['Distances']) > 0 and res['Distances'][0][0] >= 0.7:
-        #     for name, dist in zip(res['Names'], res['Distances']):
-        #         if dist[0] > conf.threshold_distance:
-        #             print(name[0], dist[0])
